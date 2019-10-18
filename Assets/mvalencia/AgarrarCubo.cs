d600ad5975cd2646fcfce868b5ec6bcfaf803df9
Marco Valencia <holi_1023@hotmail.com>
2019-10-18 09:53:55 -0400

    Agarrar Cubo

---
+++ Assets/mvalencia/AgarrarCubo.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AgarrarCubo : MonoBehaviour
+{
+    public Transform r;
+    public Transform l;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+       
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        transform.position = Vector3.Lerp(r.position, l.position, 0.1f);
+    }
+}
