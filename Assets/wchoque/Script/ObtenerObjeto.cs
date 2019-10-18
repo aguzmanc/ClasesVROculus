402c2ba42681b627a4b767e7a1d0ec10d316119a
William Choque <choquecondoriwillian@gmail.com>
2019-10-18 09:35:02 -0400

    Ejercicio GRUPO

---
+++ Assets/wchoque/Script/ObtenerObjeto.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObtenerObjeto : MonoBehaviour
+{
+    public Vector3 distancia;
+    public GameObject obj2;
+    GameObject centro;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        distancia =Vector3.Lerp(transform.position,obj2.transform.position,0.5f);
+        if (centro!=null)
+        {
+        centro.transform.position = distancia;    
+        }
+        
+        
+    }
+    private void OnTriggerEnter(Collider other) {
+        if (other.name == "ObjetoAAlzar")
+        {
+            other.gameObject.transform.parent = transform;
+            centro = other.gameObject;
+        }
+    }
+
+
+}
