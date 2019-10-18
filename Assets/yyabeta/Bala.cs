537357a9d0d7cbfd2527343c0a1083d4a47e40e2
Yamil Yabeta <xehanortn0@gmail.com>
2019-10-18 09:58:35 -0400

    Bala prefab 2

---
+++ Assets/yyabeta/Bala.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bala : MonoBehaviour
+{
+    public float speed;
+    void Start()
+    {
+        Destroy(this.gameObject,3);
+        GetComponent<Rigidbody>().AddForce(Vector3.forward*speed,ForceMode.Impulse);
+    }
+
+    void Update()
+    {
+        
+    }
+}
