201ec344fbd5a0bac2471d7620501b6b8d03827a
Marco Valencia <holi_1023@hotmail.com>
2019-10-18 10:04:18 -0400

    Agarra Cubo Arreglado

--- Assets/mvalencia/AgarrarCubo.cs
+++ Assets/mvalencia/AgarrarCubo.cs
@@ -6,24 +6,31 @@ public class AgarrarCubo : MonoBehaviour
 {
     public Transform r;
     public Transform l;
+    public float speed = 1.0F;
+
+  
+    private float startTime;
+
+   
+    private float journeyLength;
     void Start()
     {
-        
-    }
+        startTime = Time.time;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+     
+        journeyLength = Vector3.Distance(r.position, l.position);
     }
 
     private void OnTriggerEnter(Collider other)
     {
        
     }
-
     private void OnTriggerStay(Collider other)
     {
-        transform.position = Vector3.Lerp(r.position, l.position, 0.1f);
+        float distCovered = (Time.time - startTime) * speed;
+
+
+        float fractionOfJourney = distCovered / journeyLength;
+        transform.position = Vector3.Lerp(r.position, l.position, fractionOfJourney);
     }
 }
