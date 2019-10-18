ef96b57a1b60336bfc452604b26635ed4dc13b4a
Miguel Nava <minsor2010@hotmail.com>
2019-10-18 09:38:41 -0400

    Manos

---
+++ Assets/mnava/agarrado.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class agarrado : MonoBehaviour
+{
+    public GameObject medio;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other) {
+        if(other.name=="Palo")
+            other.transform.parent=medio.transform;
+    }
+}
---
+++ Assets/mnava/punto.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class punto : MonoBehaviour
+{
+    public Transform derecha;
+     public Transform izquierda;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x,y,z;
+        x=(derecha.position.x+izquierda.position.x)/2;
+         y=(derecha.position.y+izquierda.position.y)/2;
+          z=(derecha.position.z+izquierda.position.z)/2;
+          transform.position=new Vector3(x,y,z);
+        
+    }
+}
