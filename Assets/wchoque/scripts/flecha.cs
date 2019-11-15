be8392179e037eef57c3cf916eceb6d89a612f5f
Iohan Prado <asafavidan657@gmail.com>
2019-11-15 09:52:42 -0400

    flecha completa

--- Assets/wchoque/scripts/flecha.cs
+++ Assets/wchoque/scripts/flecha.cs
@@ -4,13 +4,37 @@ using UnityEngine;
 
 public class flecha : MonoBehaviour
 {
+    public float distancia=2;
+    float rotacion=5;
+    bool disparo=false;
     void Start()
     {
-        transform.GetComponent<Rigidbody>().isKinematic=false;
+        transform.GetComponent<Rigidbody>().useGravity=false;
     }
 
     void Update()
     {
-        //transform.position 
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            Transform punto_distancia = GameObject.Find("punto_distancia").GetComponent<Transform>();
+            distancia=(transform.position.magnitude-punto_distancia.position.magnitude)*20;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            disparo=true;
+            transform.GetComponent<Rigidbody>().useGravity=true;
+            Debug.Log("Disparo");
+        }
+        if (distancia>0 && disparo)
+        {
+            transform.Translate(new Vector3(0,0,distancia*Time.deltaTime), Space.Self);
+            distancia-=0.01f;
+            transform.Rotate(new Vector3(rotacion/distancia,0,0), Space.Self);
+        }
+        
+    }
+    private void OnTriggerEnter(Collider other) {
+        disparo=false;
     }
 }
