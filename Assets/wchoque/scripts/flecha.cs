019d611925d9816467b9f4e504250672be092e82
Iohan Prado <asafavidan657@gmail.com>
2019-11-15 08:51:23 -0400

    flecha

---
+++ Assets/wchoque/scripts/flecha.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class flecha : MonoBehaviour
+{
+    void Start()
+    {
+        transform.GetComponent<Rigidbody>().isKinematic=false;
+    }
+
+    void Update()
+    {
+        //transform.position 
+    }
+}
