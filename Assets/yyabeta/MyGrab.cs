18e97f5630b3ead0e5c1791e94d8da979945d9b4
Yamil Yabeta <xehanortn0@gmail.com>
2019-11-15 09:15:51 -0400

    .

---
+++ Assets/yyabeta/MyGrab.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyGrab : MonoBehaviour
+{
+    [SerializeField]
+    protected OVRInput.Controller m_controller;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
