762d9152042c8e07ac78d32f2c896c43f5962b64
Alan Lafuente <alan.alck12@gmail.com>
2019-11-15 09:23:19 -0400

    arco en mano izquierda

---
+++ Assets/Posicion.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Posicion : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject mano_zquierda;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.parent=mano_zquierda.transform;
+        transform.position=new Vector3(0,0,0);
+    }
+}
