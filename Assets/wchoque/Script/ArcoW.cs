9e81c82f1975a03693651f7894f5cbb7fa8297e9
ccw99 <choquecondoriwillian@gmail.com>
2019-11-20 11:33:01 -0400

    Arco primera prueba

---
+++ Assets/wchoque/Script/ArcoW.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArcoW : MonoBehaviour
+{
+    Renderer rend;
+    public Material materialTocado;
+    public Material materialAgarrado;
+    public Material materialSoltado;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rend= GetComponent<Renderer>();
+        rend.material=materialSoltado;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void Tocar(){
+        rend.material = materialTocado;
+        Debug.Log("toco");
+    }
+    public void DejarTocar(){
+        rend.material = materialSoltado;
+    }
+}
