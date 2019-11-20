821612a3de08f59943bffc43c63c9f7824187f53
eGalleguillos <ericgaso97@gmail.com>
2019-11-20 12:56:30 -0400

    arco y flecha eric galleguillos v2

---
+++ Assets/egalleguillos/AgarradoE.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AgarradoE : MonoBehaviour
+{
+    const float LIMITE_AGARRE = 0.7f;
+    const float LIMITE_SOLTAR = 0.3f;
+
+    [Range(0f, 1f)]
+    public float agarre;
+
+    public bool estaAgarrando;
+
+    public Arco arco;
+
+
+    void Start() {
+        estaAgarrando = false;
+    }
+
+
+    void Update()
+    {
+        bool cambio = UpdateNivelAgarre();
+
+        if(estaAgarrando && arco != null && cambio) {
+            arco.Agarrar(transform);
+        }
+
+        if(estaAgarrando==false && cambio && arco!=null){
+            arco.Soltar();
+        }
+    }
+
+
+
+
+    bool UpdateNivelAgarre(){
+        float actual = OVRInput.Get(OVRInput.Axis1D.PrimaryHandTrigger, OVRInput.Controller.LTouch);
+        bool limiteTraspasado = false;
+
+        if(agarre < LIMITE_AGARRE  && actual >= LIMITE_AGARRE){
+            estaAgarrando = true;
+            limiteTraspasado = true;
+        }
+
+        if(agarre > LIMITE_SOLTAR && actual <= LIMITE_SOLTAR){
+            estaAgarrando = false;
+            limiteTraspasado = true;
+        }
+
+        agarre = actual;
+
+        return limiteTraspasado;
+    }
+
+
+
+    void OnTriggerEnter(Collider otro) {
+        Arco arcoAgarrado = otro.GetComponent<Arco>();
+
+        if(arcoAgarrado!=null) {
+            arco = arcoAgarrado;
+            arco.Tocar();
+        }
+    }
+
+
+    void OnTriggerExit(Collider otro) {
+        Arco arcoAgarrado = otro.GetComponent<Arco>();
+        if(arcoAgarrado!=null) {
+            arco.DejarDeTocar();
+            arco = null;
+        }
+    }
+}
+
