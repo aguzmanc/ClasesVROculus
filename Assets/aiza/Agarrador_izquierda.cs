851311b4b0af5f2148aa4d1824003d71faadacea
AdrianAiza02 <adrianlastword@gmail.com>
2019-11-20 12:51:58 -0400

    arco

---
+++ Assets/aiza/Agarrador_izquierda.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Agarrador_izquierda : MonoBehaviour
+{
+    const float LIMITE_AGARRE=0.7F;
+    const float LIMITE_SOLTAR = 0.3F;
+    [Range(0f,1f)]
+    public float agarre;
+
+    public arcosc arco;
+
+    public bool estaAgarrando;
+
+    private void Start() {
+        estaAgarrando=false;
+    }
+
+    private void Update() {
+        bool cambio = ActualizarNivelAgarre();
+
+        if (estaAgarrando && arco !=null && cambio)
+        {
+            arco.Agarrar(transform);
+        }
+
+        if (estaAgarrando == false && cambio && arco !=null)
+        {
+            arco.Soltar2();
+        }
+      
+    }
+
+    bool ActualizarNivelAgarre()
+    {
+        bool limiteTraspasado = false;
+        float actual =  OVRInput.Get(OVRInput.Axis1D.PrimaryHandTrigger,OVRInput.Controller.LTouch);
+        if (agarre<LIMITE_AGARRE && actual >=LIMITE_SOLTAR)
+        {
+            estaAgarrando=true;
+            limiteTraspasado = true;
+        }
+        if (agarre > LIMITE_SOLTAR && actual <= LIMITE_SOLTAR)
+        {
+            estaAgarrando=false;
+            limiteTraspasado= true;
+        }
+     
+        agarre=actual;
+
+        return limiteTraspasado;
+    }
+
+
+
+    private void OnTriggerEnter(Collider other)
+     {
+        arcosc arcoAgarrado = other.GetComponent<arcosc>();
+        if (arcoAgarrado==null)
+        {   arco = arcoAgarrado;
+            arco.Tocar();
+        }
+    }
+
+    private void OnTriggerExit(Collider other) {
+        arcosc arcoAgarrado = other.GetComponent<arcosc>();
+        if (arcoAgarrado==null)
+        {
+
+            arco.Soltar();
+            arco = null;
+        }
+    }
+ 
+}
---
+++ Assets/aiza/arcosc.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class arcosc : MonoBehaviour
+{
+    Renderer rend;
+
+    Rigidbody body;
+    public Material MaterialTocado;
+    public Material materialAgarrad;
+    public Material materialSuelt;
+    void Start()
+    {
+        body = GetComponent<Rigidbody>();
+        rend = GetComponent<Renderer>();
+        rend.material = materialSuelt;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+      public void Soltar()
+    {
+        rend.material = materialSuelt;
+    }
+
+       public void Agarrar(Transform agarrador)
+    {
+        rend.material = materialAgarrad;
+        body.isKinematic=true;
+        transform.parent = agarrador;
+    }
+
+    public void Tocar()
+    {
+            rend.material = MaterialTocado;
+    }
+
+
+    //deja caer objeto 
+    public void Soltar2()
+    {
+        transform.parent =null;
+        rend.material = MaterialTocado;
+        body.isKinematic = false;
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        
+    }
+}
