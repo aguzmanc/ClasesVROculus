1ac7b3783413837bfa6f241303179cdd35bc1b03
Alan Lafuente <you@example.com>
2019-11-20 11:58:02 -0400

    Arco y flecha Miguel Nava

---
+++ Assets/mnava/agarraodr.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class agarraodr : MonoBehaviour
+{
+    const float limiteAgarre = 0.7f;
+    const float LimiteSuelto = 0.3f;
+    public bool estaAgarrando;
+
+    [Range (0f,1f)]
+    public float NivelAgarre;
+    public arcoMN arcoGlo;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       bool cambio=actualizarAgarre();
+       if(estaAgarrando && arcoGlo!=null)
+       {
+           arcoGlo.agarrar(transform);
+       }
+       if(estaAgarrando==false && cambio && arcoGlo!=null){
+            arcoGlo.soltar();
+        }
+    }
+    bool actualizarAgarre()
+    {
+        bool cambio=false;
+        float actual=OVRInput.Get(OVRInput.Axis1D.PrimaryHandTrigger,OVRInput.Controller.LTouch);
+        if(NivelAgarre<limiteAgarre && actual>=limiteAgarre)
+        {
+            estaAgarrando=true;
+            cambio=true;
+        }
+        if(NivelAgarre>LimiteSuelto && actual<=LimiteSuelto)
+        {
+            estaAgarrando=false;
+            cambio=false;
+        }
+        NivelAgarre=actual;
+        return cambio;
+    }
+    void OnTriggerEnter(Collider other) {
+       arcoMN arcoMano = other.GetComponent<arcoMN>();
+
+        if(arcoMano!=null) {
+            arcoGlo=arcoMano;
+            arcoGlo.tocar();
+        }
+        
+    }
+     void OnTriggerExit(Collider otro) {
+        arcoMN arcoMano = otro.GetComponent<arcoMN>();
+        if(arcoMano!=null) {
+            arcoGlo.dejarTocar();
+            arcoGlo = null;
+        }
+     }
+}
---
+++ Assets/mnava/arcoMN.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class arcoMN : MonoBehaviour
+{
+   
+    public List<Renderer> listaRe;
+    Rigidbody cuerpo;
+    Rigidbody body;
+    public Material Magarrado;
+    public Material Msuelto;
+     public Material Mtocado;
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+       
+         cuerpo=GetComponent<Rigidbody>();
+          foreach(Renderer red in listaRe)
+        {
+            red.material=Msuelto;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void tocar()
+    {
+       foreach(Renderer red in listaRe)
+        {
+            red.material=Mtocado;
+        }
+    }
+    public void dejarTocar()
+    {
+          foreach(Renderer red in listaRe)
+        {
+            red.material=Msuelto;
+        }
+    }
+      public void agarrar(Transform mano)
+    {
+        foreach(Renderer red in listaRe)
+        {
+            red.material=Magarrado;
+        }
+        transform.parent=mano;
+        cuerpo.isKinematic=true;
+
+        transform.localPosition = Vector3.zero;
+        transform.localRotation = Quaternion.identity;
+    }
+    public void soltar()
+    {
+        foreach(Renderer red in listaRe)
+        {
+            red.material=Msuelto;
+        }
+       
+        transform.parent=null;
+        cuerpo.isKinematic=false;
+    }
+}
