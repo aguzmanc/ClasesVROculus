3550590a3d07687783f39cb1de361af2f8a783eb
Iohan Prado <asafavidan657@gmail.com>
2019-11-20 11:41:59 -0400

    prueba tocar

---
+++ Assets/eaparicio/Agarrar.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Agarrar : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider otro) {
+        Debug.Log(otro.name);
+        arco arc = otro.GetComponent<arco>();
+        Debug.Log(arc);
+        if (arc!=null)
+        {
+            arc.Tocar();
+        }
+    }
+
+    void OnTriggerExit(Collider otro)
+    {
+        Debug.Log(otro.name);
+        arco arc = otro.GetComponent<arco>();
+        if (arc!=null)
+        {
+            arc.DejarTocar();
+        }
+    }
+}
