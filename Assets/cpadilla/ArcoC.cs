2e2b93a65210177953605cf97a5ef670430258a9
cpadilla <charlypadillajr@gmail.com>
2019-11-20 12:30:15 -0400

    Arco y Flecha cPadilla

---
+++ Assets/cpadilla/ArcoC.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArcoC : MonoBehaviour
+{
+    public Renderer rend;
+    Rigidbody body;
+
+    public Material materialTocado;
+    public Material materialAgarrado;
+    public Material materialSuelto; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = GetComponent<Rigidbody>();
+        rend.material = materialSuelto;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Tocar() {
+        rend.material = materialTocado;
+    }
+
+
+    public void DejarDeTocar() {
+        rend.material = materialSuelto;
+    }
+
+
+    public void Agarrar(Transform agarrador) { 
+        rend.material = materialAgarrado;
+        body.isKinematic = true;
+        transform.parent = agarrador;
+
+        transform.localPosition = Vector3.zero;
+        transform.localRotation = Quaternion.identity;
+    }
+
+
+    public void Soltar() {
+        transform.parent = null;
+        rend.material = materialTocado;
+        body.isKinematic = false;
+    }
+}
