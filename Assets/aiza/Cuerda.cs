058f79ee654094e78317d2454f15f83165706552
AdrianAiza02 <adrianlastword@gmail.com>
2019-11-22 10:08:03 -0400

    envio

---
+++ Assets/aiza/Cuerda.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cuerda : MonoBehaviour
+{
+     public Renderer rend;
+
+    public Material materialSuelto;
+    public Material materialTocado;
+    public Material materialAgarrando;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+          rend.material = materialSuelto;
+    }
+
+    // Update is called once per frame
+    public void Tocar(){
+        rend.material = materialTocado;
+    }
+
+    public void DejarDeTocar(){
+        rend.material = materialSuelto;
+    }       
+
+
+    public void Agarrar(){
+        rend.material = materialAgarrando;
+    }
+
+
+    public void Soltar(){
+        rend.material = materialTocado;
+    } 
+}
