7c73bea52b571ed56c1f4464a45ddb8898e74e20
Iohan Prado <asafavidan657@gmail.com>
2019-11-22 10:26:54 -0400

    AgarrarCuerda

---
+++ Assets/iprado/Recursos/CuerdaIP.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CuerdaIP : MonoBehaviour
+{
+    public Renderer rend;
+
+    public Material noTaken;
+    public Material canTake;
+    public Material taken;
+
+
+    void Start()
+    {
+        rend.material = noTaken;
+    }
+
+
+    void Update()
+    {
+    }
+
+
+    public void Tocar(){
+        rend.material = canTake;
+    }
+
+    public void DejarDeTocar(){
+        rend.material = noTaken;
+    }       
+
+
+    public void Agarrar(){
+        rend.material = taken;
+    }
+
+
+    public void Soltar(){
+        rend.material = canTake;
+    } 
+
+}
