644d972c398a4a2735e5727fcd965188843dc4e1
Yamil Yabeta <xehanortn0@gmail.com>
2019-11-27 12:33:23 -0400

    2.3

---
+++ Assets/yyabeta/Flecha.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flecha : MonoBehaviour
+{
+    Rigidbody rigidbody;
+    void Start()
+    {
+        rigidbody=GetComponent<Rigidbody>();
+    }
+    private void OnCollisionEnter(Collision other) {
+        if(other.gameObject.tag=="Pared")
+        {
+            rigidbody.constraints=RigidbodyConstraints.FreezeAll;
+        }
+    }
+}
