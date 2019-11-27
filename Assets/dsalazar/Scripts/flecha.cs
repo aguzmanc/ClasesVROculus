55fcca50539f157da3fb9a1a56aa133229c342eb
diegosc <diegosc1512@gmail.com>
2019-11-27 13:06:33 -0400

    correccion

--- Assets/dsalazar/Scripts/flecha.cs
+++ Assets/dsalazar/Scripts/flecha.cs
@@ -19,6 +19,8 @@ public class flecha : MonoBehaviour
         if (other.gameObject.tag=="circulo")
         {
         GetComponent<Rigidbody>().useGravity=false;
+        GetComponent<Rigidbody>().isKinematic=true;
+        
         }
     }
 }
