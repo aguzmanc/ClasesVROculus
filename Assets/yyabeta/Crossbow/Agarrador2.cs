be45627f5cc4c6277f6ac8d18f33fbf86dc9302c
Yamil Yabeta <xehanortn0@gmail.com>
2019-11-29 08:26:26 -0400

    Ballesta 1.0

---
+++ Assets/yyabeta/Crossbow/Agarrador2.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Agarrador2 : MonoBehaviour
+{
+   const float LIMITE_AGARRE = 0.7f;
+    const float LIMITE_SOLTAR = 0.3f;
+
+    [Range(0f, 1f)]
+    public float agarre;
+
+    public bool estaAgarrando;
+
+    public Ballesta ballesta;
+
+    void Start() {
+        estaAgarrando = false;
+    }
+
+
+    void Update()
+    {
+        bool cambio = UpdateNivelAgarre();
+
+        if(estaAgarrando && ballesta != null && cambio) {
+            ballesta.Agarrar(transform);
+            if(OVRInput.Get(OVRInput.Axis1D.PrimaryIndexTrigger, OVRInput.Controller.LTouch)==1)
+            {
+                ballesta.DisparaFlecha(100);
+            }
+        }
+
+        if(estaAgarrando==false && cambio && ballesta!=null){
+            ballesta.Soltar();
+        }
+
+        
+    }
+
+    bool UpdateNivelAgarre(){
+        float actual = OVRInput.Get(OVRInput.Axis1D.PrimaryHandTrigger, OVRInput.Controller.LTouch);
+        bool limiteTraspasado = false;
+
+        if(agarre < LIMITE_AGARRE  && actual >= LIMITE_AGARRE){
+            estaAgarrando = true;
+            limiteTraspasado = true;
+        }
+
+        if(agarre > LIMITE_SOLTAR && actual <= LIMITE_SOLTAR){
+            estaAgarrando = false;
+            limiteTraspasado = true;
+        }
+
+        agarre = actual;
+
+        return limiteTraspasado;
+    }
+
+
+
+    void OnTriggerEnter(Collider otro) {
+        Ballesta ballestaAgarrada = otro.GetComponent<Ballesta>();
+
+        if(ballestaAgarrada!=null) {
+            ballesta = ballestaAgarrada;
+            ballesta.Tocar();
+        }
+    }
+
+
+    void OnTriggerExit(Collider otro) {
+        Arco arcoAgarrado = otro.GetComponent<Arco>();
+        if(arcoAgarrado!=null) {
+            ballesta.DejarDeTocar();
+            ballesta = null;
+        }
+    }
+}
---
+++ Assets/yyabeta/Crossbow/Ballesta.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ballesta : MonoBehaviour
+{
+    [Header("Ballesta")]
+    public Renderer rend;
+    Rigidbody body;
+
+    public Material materialTocado;
+    Material materialAgarrado;
+    public Material materialSuelto;
+
+
+    [Header("Flecha")]
+    public GameObject prfabFlecha;
+
+    public Transform disparador;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = GetComponent<Rigidbody>();
+        materialAgarrado = rend.material;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Tocar() {
+        rend.material = materialTocado;
+    }
+
+
+    public void DejarDeTocar() {
+        rend.material = materialSuelto;
+    }
+
+    public void Agarrar(Transform agarrador) { 
+        rend.material = materialAgarrado;
+        body.isKinematic = true;
+        transform.parent = agarrador;
+        transform.localPosition = Vector3.zero;
+        transform.localRotation = Quaternion.identity;
+    }
+
+    public void DisparaFlecha(float speed)
+    {
+        GameObject f = Instantiate(prfabFlecha,disparador.position,disparador.rotation);
+        f.GetComponent<Rigidbody>().AddForce(disparador.transform.forward*speed*3f);
+        Destroy(f,3);
+    }
+    public void Soltar() 
+     {
+        transform.parent = null;
+        rend.material = materialTocado;
+        body.isKinematic = false;
+    }
+
+}
