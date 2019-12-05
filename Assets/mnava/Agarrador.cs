b9fa559339157c31eb15fdce57af5dad333fec79
Miguel <minsor2010@hotmail.com>
2019-12-05 16:33:30 -0400

    Miguel: armas de una mano agarrables

---
+++ Assets/mnava/Agarrador.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Agarrador : MonoBehaviour
+{
+    const float limiteAgarre = 0.7f;
+    const float LimiteSuelto = 0.3f;
+    public bool estaAgarrando;
+
+    public bool AGARREF;
+    [Range (0f,1f)]
+    public float NivelAgarre;
+    public ArmaUnaMano arma;
+
+
+
+    void Start()
+    {
+        
+    }
+
+   
+    void Update()
+    {
+
+       bool cambio=actualizarAgarre();
+
+
+       estaAgarrando=AGARREF;//agarre forzado 
+       if(estaAgarrando && arma!=null)
+       {
+           arma.agarrar(transform);
+       }
+       if(estaAgarrando==false && cambio && arma!=null){
+            arma.soltar();
+        }
+    }
+    bool actualizarAgarre()
+    {
+        bool cambio=false;
+        float actual=OVRInput.Get(OVRInput.Axis1D.PrimaryHandTrigger,OVRInput.Controller.LTouch);
+        if(NivelAgarre<limiteAgarre && actual>=limiteAgarre)
+        {
+            estaAgarrando=true;
+            cambio=true;
+        }
+        if(NivelAgarre>LimiteSuelto && actual<=LimiteSuelto)
+        {
+            estaAgarrando=false;
+            cambio=true;
+        }
+        NivelAgarre=actual;
+        return cambio;
+    }
+    void OnTriggerEnter(Collider other) {
+        if(other.tag=="Arma1")
+        {
+            ArmaUnaMano armaTocada = other.GetComponent<ArmaUnaMano>();
+            if(armaTocada!=null) 
+            {
+                arma=armaTocada;
+                arma.tocar();
+            }
+        }
+        
+    }
+     void OnTriggerExit(Collider other) {
+        if(other.tag=="Arma1")
+        {
+            ArmaUnaMano armaTocada = other.GetComponent<ArmaUnaMano>();
+            if(armaTocada!=null)
+            {
+                arma.dejarTocar();
+                arma = null;
+            }
+        }
+     }
+}
---
+++ Assets/mnava/ArmaUnaMano.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArmaUnaMano : MonoBehaviour
+{
+    public Renderer indicador;
+    Rigidbody cuerpo;
+
+    public Material EstadoAgarrado;
+    public Material EstadoSuelto;
+    public Material EstadoTocado;
+    void Start()
+    {
+        cuerpo=GetComponent<Rigidbody>();
+        indicador.material=EstadoSuelto;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+     public void tocar()
+    {
+        indicador.material=EstadoTocado;
+       
+    }
+    public void dejarTocar()
+    {
+        indicador.material=EstadoSuelto;
+       
+    }
+    public void agarrar(Transform mano)
+    {
+ 
+        indicador.material=EstadoAgarrado;
+        transform.parent=mano;
+        cuerpo.isKinematic=true;
+        transform.localPosition = Vector3.zero;
+        transform.localRotation =Quaternion.identity;
+        
+    }   
+    public void soltar()
+    {
+        indicador.material=EstadoSuelto;
+        transform.parent=null;
+        cuerpo.isKinematic=false;
+    }
+}
