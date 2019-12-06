ad445308e5e5e699286f03f7c7169e2d6dec3e3a
Yamil Yabeta <xehanortn0@gmail.com>
2019-12-06 09:38:09 -0400

    Scripts Base 2

--- Assets/yyabeta/Proyecto/Scripts/ObjetoAgarrable.cs
+++ Assets/yyabeta/Proyecto/Scripts/ObjetoAgarrable.cs
@@ -2,12 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ObjetoAgarrable : MonoBehaviour
+public abstract class ObjetoAgarrable : MonoBehaviour
 {
+    Rigidbody rigidbody = null;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidbody=GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
