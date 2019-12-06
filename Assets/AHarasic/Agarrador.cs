f236c386f7583bf50cd6af8e6f6b5bd40a1d0d84
Alan <alanharasic@gmail.com>
2019-12-06 09:39:43 -0400

    bola

---
+++ Assets/AHarasic/Agarrador.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Agarrador : MonoBehaviour
+{
+
+    const float LIMITE_AGARRE = 0.7f;
+    const float LIMITE_SOLTAR = 0.3f;
+
+    [Range(0f,1f)]
+    public float agarre;
+    public Bola bola;
+
+
+    public bool estaAgarrando;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+         estaAgarrando=false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    bool cambio = UpdateNivelAgarre();
+
+      if(estaAgarrando && bola!=null && cambio)
+      {
+            bola.Agarrar(transform);
+      }
+      if(estaAgarrando==false && cambio && bola!=null)
+      {
+        bola.Soltar();
+      }
+    }
+
+
+    bool UpdateNivelAgarre()
+    {
+        bool limiteTraspasado=false;
+        float actual=OVRInput.Get(OVRInput.Axis1D.PrimaryHandTrigger,OVRInput.Controller.LTouch);
+        //float actual=0.8f;
+    if( agarre<LIMITE_AGARRE && actual>=LIMITE_AGARRE)
+    {
+            estaAgarrando=true;
+            limiteTraspasado=true;
+    }
+            if(agarre>LIMITE_SOLTAR && actual <= LIMITE_SOLTAR)
+            {
+                estaAgarrando=false;
+                limiteTraspasado=true;
+                
+            }
+            agarre=actual;
+            //Debug.Log(actual);
+        return limiteTraspasado;
+        
+    }
+
+
+    private void OnTriggerEnter(Collider other) {
+       Bola bolaAgarrada = other.GetComponent<Bola>();
+       if(bolaAgarrada!=null)
+       {
+           bola=bolaAgarrada;
+           bola.Tocar();
+       }
+       // Debug.Log(other.name);
+    }
+
+
+     private void OnTriggerExit(Collider other) {
+         Bola bolaAgarrada = other.GetComponent<Bola>();
+         if(bolaAgarrada!=null)
+       {
+           
+           bola.DejarDeTocar();
+           bola =null;
+       }
+    }
+
+
+
+}
---
+++ Assets/AHarasic/Bola.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bola : MonoBehaviour
+{
+
+    public Material materialAgarrado;
+    public Material materialSuelto;
+    public Material materialTocado;
+
+    Rigidbody body;
+    public Renderer rend;
+    // Start is called before the first frame update
+    void Start()
+    {
+        body= GetComponent<Rigidbody>();
+       // rend = GetComponent<Renderer>();??
+        rend.material=materialSuelto;
+        //cuerda= transform.Find("pivot").gameObject;
+        //cuerda.SetActive(false);
+    }
+
+    // Update is called once per frame
+
+
+
+    public void Tocar(){
+        rend.material=materialTocado;
+
+    }
+
+    public void DejarDeTocar()
+    {
+        rend.material=materialSuelto;
+    }
+
+    public void Agarrar(Transform agarrador)
+    {
+        rend.material = materialAgarrado;
+        body.isKinematic = true;
+        transform.parent = agarrador;
+
+        transform.localPosition = Vector3.zero;
+        transform.localRotation = Quaternion.identity;
+       // cuerda.SetActive(true);
+        //Debug.Log("SIUUU");
+    
+    }
+
+
+            public void Soltar()
+            {
+            transform.parent=null;
+            rend.material=materialTocado;
+            body.isKinematic=false;
+           // cuerda.SetActive(false);
+            //Debug.Log("DISPARO!");
+            }
+
+
+    void Update()
+    {
+        
+    }
+}
