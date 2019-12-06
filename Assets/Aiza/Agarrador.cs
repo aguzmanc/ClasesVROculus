a233bd15e483138d2448d4e470482e071f5ac18a
adrian2010 <adrianlastword@gmail.com>
2019-12-06 00:47:22 -0400

    proyecto fase 1

---
+++ Assets/Aiza/Agarrador.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Agarrador : MonoBehaviour
+{
+    const float LIMITE_AGARRE=0.7F;
+    const float LIMITE_SOLTAR = 0.3F;
+
+    [Range(0f,1f)]
+    public float agarre;
+
+    public bool estaAgarrando;
+    public Taco taco;
+    // Start is called before the first frame update
+    void Start()
+    {
+        estaAgarrando=false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool cambio = UpdateNivelAgarre();
+
+        if (estaAgarrando && taco !=null && cambio)
+        {
+            taco.Agarrar(transform);
+        }
+        if (estaAgarrando == false && cambio && taco !=null)
+        {
+            taco.Soltar();
+        }
+    }
+
+
+    bool UpdateNivelAgarre()
+    {
+        float actual = OVRInput.Get(OVRInput.Axis1D.PrimaryHandTrigger,OVRInput.Controller.RTouch);
+        bool limiteTraspado = false;
+        if (agarre < LIMITE_AGARRE && actual >= LIMITE_AGARRE)
+        {
+            estaAgarrando=true;
+            limiteTraspado=true;
+        }
+        if (agarre > LIMITE_SOLTAR && actual <= LIMITE_SOLTAR)
+        {
+            estaAgarrando=false;
+            limiteTraspado=true;
+        }
+        agarre = actual;
+        return limiteTraspado;
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        Taco tacoAgarrado =  other.GetComponent<Taco>();
+        if (tacoAgarrado!=null)
+        {
+            taco = tacoAgarrado;
+            taco.Tocar();
+        }
+    }
+
+    private void OnTriggerExit(Collider other) {
+        Taco tacoAgarrado =  other.GetComponent<Taco>();
+        if (tacoAgarrado!=null)
+        {
+            taco.DejarDeTocar();
+            taco = null;
+        }
+    }
+}
---
+++ Assets/Aiza/Taco.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Taco : MonoBehaviour
+{
+    Rigidbody bodyTaco;
+    Renderer rendTaco;
+    public Material materialTocado;
+
+    public Material materialAgarrado;
+    public Material materialSuelto;
+    // Start is called before the first frame update
+    void Start()
+    {
+        bodyTaco = GetComponent<Rigidbody>();
+        rendTaco  = GetComponent<Renderer>();
+        rendTaco.material = materialSuelto;
+        
+    }
+
+    // Update is called once per frame
+
+
+    public void Tocar()
+    {
+        rendTaco.material = materialTocado;
+         
+    }
+
+    public void Agarrar(Transform agarrador)
+    {
+        rendTaco.material = materialAgarrado;
+        bodyTaco.isKinematic=true;
+        transform.parent = agarrador;
+
+        transform.localPosition = Vector3.zero;
+        transform.localRotation = Quaternion.Euler(90,0,0);
+    }
+
+     public void DejarDeTocar()
+    {
+         rendTaco.material = materialSuelto;
+    }
+
+     public void Soltar()
+    {
+         transform.parent=null;
+         rendTaco.material = materialTocado;
+         bodyTaco.isKinematic = false; //debatir porq lo soltare y quiero q siga vertical
+    }
+}
