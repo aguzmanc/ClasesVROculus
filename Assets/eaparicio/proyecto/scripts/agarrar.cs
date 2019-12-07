55234fd59e770ba9c775048dfc7df30b2a97a4bc
Elian Aparicio <gonzalo.gui.71@gmail.com>
2019-12-07 13:42:23 -0400

    bate

---
+++ Assets/eaparicio/proyecto/scripts/agarrar.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class agarrar : MonoBehaviour
+{
+    public float LIMITE_AGARRE=0.7F;
+    public float LIMITE_SOLTAR=0.3F;
+    public bool estaAgarrado;
+
+    public bate bate;
+
+
+     [Range(0f, 1f)]
+    public float agarre;
+     [Range(0f, 1f)]
+    public float actu;
+
+
+    public bool cambio;
+    public bool tocado;
+
+    void Start()
+    {
+        tocado=false;
+        estaAgarrado=false;
+    }
+
+    void Update()
+    {
+        cambio = UpdateAgarre();
+        if( bate!=null)
+        {
+            if (estaAgarrado && cambio && tocado)
+            {
+                bate.Agarrar(transform);
+            }
+            if(!estaAgarrado && cambio && !tocado){
+                bate.Soltar();
+            }
+        }
+       
+    }
+
+    bool UpdateAgarre(){
+
+        //PRUEBAS PC
+        float actual = OVRInput.Get(OVRInput.Axis1D.PrimaryHandTrigger, OVRInput.Controller.LTouch);
+
+        bool limitePasado=false;
+        if (agarre<LIMITE_AGARRE && actu>=LIMITE_AGARRE)
+        {
+            estaAgarrado=true;
+            limitePasado=true;
+        }
+        if (agarre>LIMITE_SOLTAR && actu <=LIMITE_SOLTAR)
+        {
+            estaAgarrado=false;
+            limitePasado=true;
+        }
+        agarre=actu;
+        return limitePasado;
+
+
+
+        //PRUEBAS VR
+        // float actual = OVRInput.Get(OVRInput.Axis1D.PrimaryHandTrigger, OVRInput.Controller.LTouch);
+
+        // bool limitePasado=false;
+        // if (agarre<LIMITE_AGARRE && actual>=LIMITE_AGARRE)
+        // {
+        //     estaAgarrado=true;
+        //     limitePasado=true;
+        // }
+        // if (agarre>LIMITE_SOLTAR && actual <=LIMITE_SOLTAR)
+        // {
+        //     estaAgarrado=false;
+        //     limitePasado=true;
+        // }
+        // agarre=actual;
+        // return limitePasado;
+    }
+    void OnTriggerEnter(Collider otro) {
+        Debug.Log(otro.name);
+        tocado=true;
+        if (otro.name=="agarre")
+        {
+            bate b = otro.GetComponent<bate>();
+            if (b!=null)
+            {
+                bate = b;
+                b.Tocar();
+            }
+        }
+    }
+
+    void OnTriggerExit(Collider otro)
+    {
+        tocado=false;
+        Debug.Log(otro.name);
+         if (otro.name=="agarre")
+        {
+            bate b = otro.GetComponent<bate>();
+            if (b!=null)
+            {
+                bate = b;
+                b.DejarTocar();
+            }
+        }
+    }
+}
---
+++ Assets/eaparicio/proyecto/scripts/bate.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bate : MonoBehaviour
+{
+    public Renderer render;
+    Rigidbody body;
+    public Material matSuelto;
+    public Material matTocado;
+    public Material matAgarrado;
+    
+    void Start()
+    {
+        body = transform.parent.GetComponent<Rigidbody>();
+        //render = GetComponent<Renderer>();
+        render.material= matSuelto;
+    }
+    public void Tocar(){
+        render.material = matTocado;
+    }
+    public void DejarTocar(){
+        render.material =matSuelto;
+    }
+    public void Agarrar(Transform agarrador){
+        render.material =matAgarrado;
+        //body.isKinematic = true;
+        transform.parent = agarrador;
+
+
+    }
+    public void Soltar(){
+        transform.parent = null;
+        //body.isKinematic = false;
+        render.material = matTocado;
+    }
+    void OnTriggerEnter(Collider other) {
+        if (other.tag=="piso")
+        {
+            body.isKinematic = true;
+            transform.position = new Vector3(0.736f,1.398f,0.053f);
+            transform.rotation = Quaternion.identity;
+        }
+    }
+}
