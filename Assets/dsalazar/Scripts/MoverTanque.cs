1be1af5fc0a0f5f010b6e141cebd7ac115b1c297
diegosc <diegosc1512@gmail.com>
2019-12-07 13:02:35 -0400

    prueba

--- Assets/dsalazar/Scripts/MoverTanque.cs
+++ Assets/dsalazar/Scripts/MoverTanque.cs
@@ -10,7 +10,9 @@ public class MoverTanque : MonoBehaviour
 
    public GameObject rPalanca;
    public GameObject lPalanca;
-   public GameObject centro;
+   public GameObject Rcentro;
+   public GameObject Lcentro;
+
     void Start()
     {
         
@@ -19,19 +21,23 @@ public class MoverTanque : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-          distanciaR = Vector3.Distance(rPalanca.transform.position, centro.transform.position);
-          distanciaL = Vector3.Distance(lPalanca.transform.position, centro.transform.position);
+          distanciaR = Vector3.Distance(rPalanca.transform.position, Rcentro.transform.position);
+          distanciaL = Vector3.Distance(lPalanca.transform.position, Lcentro.transform.position);
         if (distanciaL>0.315F&&distanciaR>0.315f)
         {
             transform.Translate(Vector3.forward * Time.deltaTime);
         }
         else if(distanciaL>0.315f&&distanciaR<0.315f)
         {
-            transform.Rotate(0.0f,1.0f*Time.deltaTime, 0.0f, Space.World);
+            transform.Rotate(0.0f,0.7f*Time.deltaTime, 0.0f, Space.World);
         }
           else if(distanciaL<0.315f&&distanciaR>0.315f)
         {
-            transform.Rotate(0.0f,-1.0f, 0.01f*Time.deltaTime, Space.World);
+            transform.Rotate(0.0f,-0.7f, 0.01f*Time.deltaTime, Space.World);
         }
+        // Vector3 thePosition = transform.TransformPoint(2, 0, 0);
+      //  Instantiate(someObject, thePosition, someObject.transform.rotation);
+
+
     }
 }
