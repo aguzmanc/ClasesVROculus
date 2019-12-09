8c8b3dc8c66bf7af14f8dca26e5e5d0781a676f0
Alan Lafuente <you@example.com>
2019-12-09 09:05:01 -0400

    Prueba lanzamiento y daño 2

--- Assets/mnava/Vida.cs
+++ Assets/mnava/Vida.cs
@@ -1,20 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Vida : MonoBehaviour
 {
     // Start is called before the first frame update
     int vida;
+    Text vidaTexto;
+    public GameObject textual;
     void Start()
     {
         vida=100;
+        vidaTexto=textual.GetComponent<Text>();
     }
 
     // Update is called once per frame
     void Update()
     {
         Debug.Log(vida);
+        vidaTexto.text="Vida: "+vida;
+       
+
     }
     public void bajarVida(int daño)
     {
