d2b96a45e97467bc6d2fd377824c83c8ed9c28f7
Alan Lafuente <you@example.com>
2019-12-09 09:23:57 -0400

    Prueba lanzamiento y daño 4

--- Assets/mnava/AgarradorManoApoyo.cs
+++ Assets/mnava/AgarradorManoApoyo.cs
@@ -29,7 +29,7 @@ public class AgarradorManoApoyo : MonoBehaviour
           bool cambio=actualizarAgarre();
 
 
-       //estaAgarrando2=AGARREF;//agarre forzado 
+       estaAgarrando2=AGARREF;//agarre forzado 
 
        if(tipoArma==1)
        {
