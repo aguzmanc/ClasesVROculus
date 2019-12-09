ce3dc9025478adead39cb41c34ecd8d9f66abcec
Eric emmanuel <ericgaso97@gmail.com>
2019-12-09 11:01:51 -0400

    proyecto: caja and scripts ballestas

---
+++ Assets/egalleguillos/Scripts/CajaMuerte.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CajaMuerte : MonoBehaviour
+{
+    public float timer = 0;
+    Rigidbody rigidbody;
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if((int)timer == 5){
+            rigidbody.useGravity = true;
+        }
+    }
+
+    void OnTriggerEnter(Collider c) {
+        if(c.tag == "Player"){
+            transform.GetChild(0).gameObject.SetActive(false);
+        }
+
+    }
+}
