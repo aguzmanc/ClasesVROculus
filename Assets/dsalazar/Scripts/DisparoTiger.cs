ba0b349d0bbdcedd1c2cde556c8d3b922778454b
diegosc <diegosc1512@gmail.com>
2019-12-09 01:04:45 -0400

    varias cosas nuevas

---
+++ Assets/dsalazar/Scripts/DisparoTiger.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisparoTiger : MonoBehaviour
+{
+     public GameObject cannonPivotBone;
+    public GameObject bullet;
+    public float shootCooldown = 1;
+    public float strength = 50;
+    public bool cargado=true;
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+           if (Input.GetKeyDown(KeyCode.Space)&&cargado) {
+            Shoot();
+            cargado=false;
+        } 
+        if (OVRInput.GetDown(OVRInput.Button.SecondaryIndexTrigger)&&cargado) {
+            Shoot();
+            cargado=false;
+        }
+         if (OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger)) {
+           // Shoot();
+            cargado=true;
+        }
+
+    }
+       public void Shoot () {
+        GameObject createdBullet = Instantiate(bullet);
+        createdBullet.transform.position = cannonPivotBone.transform.position;
+        Rigidbody body = createdBullet.GetComponent<Rigidbody>();
+        body.AddForce(cannonPivotBone.transform.forward * strength,
+                      ForceMode.Impulse);
+    }
+}
