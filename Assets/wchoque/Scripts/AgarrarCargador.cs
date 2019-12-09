0651fb5093690a8cbf441f49ca9c000661ac49fd
choqueWilliam <choquecondoriwillian@gmail.com>
2019-12-09 09:28:32 -0400

    Prueba v1

--- Assets/wchoque/Scripts/AgarrarCargador.cs
+++ Assets/wchoque/Scripts/AgarrarCargador.cs
@@ -2,35 +2,35 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ArradarArma : MonoBehaviour
+public class AgarrarCargador : MonoBehaviour
 {
-    public Arma armaMano;
-    const float limite_Agarre=0.7f;
+    public CargadorArma cargadorArma;
+    // Start is called before the first frame update
+        const float limite_Agarre=0.7f;
     const float limite_Soltar=0.3f;
     [Range(0,1)]
     public float agarre;
     bool cambio;
     public bool estaAgarrando;
     float actual;
-    // Start is called before the first frame update
     void Start()
     {
-        estaAgarrando =false;
+        
     }
 
     // Update is called once per frame
     void Update()
     {
         cambio = UpdateNivelAgarre();
-//cambio = true;
-        if(estaAgarrando&& armaMano!=null && cambio){
-            armaMano.Agarrar(transform);
+       // cambio = true;
+        if(estaAgarrando&& cargadorArma!=null && cambio){
+            cargadorArma.Agarrar(transform);
         }
-        if(!estaAgarrando&&cambio&&armaMano!=null){
-            armaMano.soltar();
+        if(!estaAgarrando&&cambio&&cargadorArma!=null){
+            cargadorArma.soltar();
         }
     }
-    bool UpdateNivelAgarre(){
+      bool UpdateNivelAgarre(){
         actual = OVRInput.Get(OVRInput.Axis1D.PrimaryHandTrigger,OVRInput.Controller.LTouch);
         bool limiteTraspasado=false;
         if(agarre<limite_Agarre && actual>=limite_Agarre){
@@ -44,18 +44,13 @@ public class ArradarArma : MonoBehaviour
     agarre = actual;
     return limiteTraspasado;
     }
-    
-    /// <summary>
-    /// OnTriggerEnter is called when the Collider other enters the trigger.
-    /// </summary>
-    /// <param name="other">The other Collider involved in this collision.</param>
-    void OnTriggerEnter(Collider other)
+     void OnTriggerEnter(Collider other)
     {
-        if(other.tag == "Arma"){
-            Arma armaV = other.GetComponent<Arma>();
-            if(armaV!=null){
-                armaMano = armaV;
-                armaMano.Tocar(); 
+        if(other.tag == "Cargador"){
+            CargadorArma cargaArma = other.GetComponent<CargadorArma>();
+            if(cargaArma!=null){
+                cargadorArma = cargaArma;
+                cargadorArma.Tocar(); 
             }
         }
     }
@@ -65,12 +60,12 @@ public class ArradarArma : MonoBehaviour
     /// <param name="other">The other Collider involved in this collision.</param>
     void OnTriggerExit(Collider other)
     {
-         if(other.tag == "Arma"){
-            Arma armaV = other.GetComponent<Arma>();
-            if(armaV!=null){
-                armaMano = armaV;
-                armaMano.DegarTocar();
-                armaMano=null; 
+         if(other.tag == "Cargador"){
+            CargadorArma cargaArma = other.GetComponent<CargadorArma>();
+            if(cargaArma!=null){
+                cargadorArma = cargaArma;
+                cargadorArma.DegarTocar();
+                cargadorArma=null; 
             }
         }
     }
