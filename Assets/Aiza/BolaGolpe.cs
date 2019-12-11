ea7a803399871405427cbe8390428b9623026c81
adrian2010 <adrianlastword@gmail.com>
2019-12-10 23:02:44 -0400

    bola disparada

---
+++ Assets/Aiza/BolaGolpe.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BolaGolpe : MonoBehaviour
+{
+    public Rigidbody rb;
+    private void OnTriggerEnter(Collider other) {
+        if (other.tag == "Bola")
+        {
+            print("entro");
+            rb.AddForce(this.transform.parent.forward * 2,ForceMode.Impulse);
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
