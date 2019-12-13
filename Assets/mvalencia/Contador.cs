75ab059d782e2a27d2caec16ca6b1afdabe5073f
marco <holi_1023@hotmail.com>
2019-12-13 09:22:47 -0400

    Final1

---
+++ Assets/mvalencia/Contador.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Contador : MonoBehaviour
+{
+    Text text;
+    float theTime;
+    public float speed = 1;
+    bool playing;
+    void Start()
+    {
+        text = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        theTime += Time.deltaTime * speed;
+        string hours = Mathf.Floor((theTime % 216000) / 3600).ToString("00");
+        string minutes = Mathf.Floor((theTime % 3600) / 60).ToString("00");
+        string seconds = (theTime % 60).ToString("00");
+        text.text = hours + ":" + minutes + ":" + seconds;
+
+    }
+}
